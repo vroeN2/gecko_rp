48db451ab9b3951777f388e32f3178fa30e0d512
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2023-01-22 19:03:05 +0100

    feat: add major part of page
    * add icons used in project
    * add images used in project
    * add extra small font size
    * define blue color as variable
    * move styles from _styles to _base
    * add script for cookies bar
    * create scss functions

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+const cookies = document.getElementById("cookies");
+const button = document.getElementById("cookies__agree");
+
+button.addEventListener("click", () => (cookies.style.opacity = 0));
